02df47bf70632b866c4bf7f1b078654b2e413138
Syed Shean <shean.syed@gmail.com>
2017-05-24 12:39:55 +1000

    User List

--- ENETCareMVCApp/Controllers/UsersController.cs
+++ ENETCareMVCApp/Controllers/UsersController.cs
@@ -24,6 +24,7 @@ namespace ENETCareMVCApp.Controllers
         // GET: Users/Details/5
         public ActionResult Details(int? id)
         {
+  
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
